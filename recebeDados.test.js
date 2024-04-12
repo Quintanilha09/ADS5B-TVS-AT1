85ca593b1284b492accc64dfae64217ec71658de
Gabriel Cabral <gabriel.hemendinger@gmail.com>
2024-04-12 18:22:13 -0300

    primeiro commit

--- recebeDados.test.js
+++ recebeDados.test.js
@@ -2,9 +2,9 @@
 const dadosEstudante = require('./dadosEstudante');
 
 describe('Teste da função recebeDados', () => {
-  test('Verifica se a função processa corretamente o nome "Mellinda Gates" e o RA "228697"', () => {
-    const nome = "Mellinda Gates";
-    const RA = "228697";
+  test('Verifica se a função processa corretamente o nome "Gabriel Quintanilha" e o RA "061808"', () => {
+    const nome = "Gabriel Quintanilha";
+    const RA = "061808";
     const esperado = `Dados recebidos - Nome: ${nome}, RA: ${RA}`;
 
     expect(dadosEstudante(nome, RA)).toBe(esperado);
